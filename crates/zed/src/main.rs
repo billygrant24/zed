4bfa3e7256e6c23c9d0d831e0335ee5623410bed
Billy Grant <billygrant24@icloud.com>
2026-07-31 11:12:42 +0100

    Restore macOS title bar initialization

--- crates/zed/src/main.rs
+++ crates/zed/src/main.rs
@@ -609,6 +609,7 @@ fn main() {
         theme_selector::init(cx);
         settings_profile_selector::init(cx);
         language_tools::init(cx);
+        title_bar::init(cx);
         git_ui::init(cx);
         feedback::init(cx);
         markdown_preview::init(cx);
